2a995fbfe52d447f3c5c7dddf98c4f25780b12cd
Henrik Blidh <henrik.blidh@nedomkull.com>
2015-08-05 00:33:35 +0200

    Login is now up and running.

--- wlmetricsweb/__init__.py
+++ wlmetricsweb/__init__.py
@@ -3,9 +3,11 @@
 
 from flask import Flask
 from flask.ext.stormpath import StormpathManager
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
 app.config.from_object('wlmetricsweb.config')
 stormpath_manager = StormpathManager(app)
+mongodb = PyMongo(app)
 
 from wlmetricsweb import views, filters, forms
--- wlmetricsweb/views.py
+++ wlmetricsweb/views.py
@@ -20,11 +20,13 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 from flask import render_template, jsonify, request, redirect, send_file
+from flask.ext.stormpath import login_required, user, groups_required
 
-from wlmetricsweb import app, forms, config
-
+from wlmetricsweb import app, forms, config, stormpath_manager, mongodb
 
 @app.route('/', methods=['GET', ])
 def index():
-    return render_template('index.html',
-                           title='Home')
+    if user.is_authenticated():
+        return render_template('index.html', title='{0} {1}'.format(user.given_name, user.surname))
+    else:
+        return render_template('landing.html', title='Home')
